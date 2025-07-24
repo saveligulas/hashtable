ee9aa301aed5d42b931b71bc710877684411db8d
saveli <saveli@LAPTOP-SH6L8DA8.home>
2025-07-24 12:26:00 +0200

    added hash table and pair lifecycle

---
+++ src/hash_pair_lifecycle.c
@@ -0,0 +1,7 @@
+#include "hash_table.h"
+#include "hash_table_internal.h"
+
+extern HashPair_t* hash_table_pair_create(void* key, size_t key_length, void* value, size_t value_length) {
+    size_t hash_pair_offset = key_length;
+    
+}
---
+++ src/hash_table_lifecycle.c
@@ -0,0 +1,5 @@
+#include "hash_table.h"
+
+extern HashTable_t* hash_table_create(int initialCapacity) {
+
+}
